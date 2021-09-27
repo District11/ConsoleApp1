9c8ce2d3511689f0ecc691846e6609cd6c3a235d
District_11 <baira.98@mail.ru>
2021-09-27 15:45:41 +0400

    Изменения в соответствии с замечаниями

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,14 +7,25 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // значение первой стороны прямоугольника
+            double side1, side2;
             Console.WriteLine("Введите значение длинны прямоугольника: ");
-            double side1 =  Convert.ToDouble(Console.ReadLine());
+            string inputSide1 = Console.ReadLine();
             
             Console.WriteLine("Введите значение ширины прямоугольника: ");
-            double side2 =  Convert.ToDouble(Console.ReadLine());
-            
-            var rectangle = new Rectangle(side1, side2);
-            Console.WriteLine("Площадь прямоугольника: {0} Периметр прямоугольника: {1}", rectangle.Area, rectangle.Perimetr);
+            string inputSide2 = Console.ReadLine();
+
+            double.TryParse(inputSide1, out side1);
+            double.TryParse(inputSide2, out side2);
+            if (double.TryParse(inputSide1, out side1) & double.TryParse(inputSide2, out side2))
+            {
+                Console.WriteLine("Преобразование успешно");
+                var rectangle = new Rectangle(side1, side2);
+                Console.WriteLine("Площадь прямоугольника: {0} Периметр прямоугольника: {1}", rectangle.Area, rectangle.Perimetr);
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
--- ConsoleApp1/Rectangle.cs
+++ ConsoleApp1/Rectangle.cs
@@ -2,18 +2,25 @@
 
 namespace ConsoleApp1
 {
+    /// <summary>
+    /// Класс прямоугольника
+    /// </summary>
     public class Rectangle
     {
         // Значнеие первой сторорны прямоугольника
-        double side1;
+       private readonly double _side1;
         // Значнеие второй сторорны прямоугольника
-        double side2;
+       private readonly double _side2;
         
-        
+        /// <summary>
+        /// Конструктор прямоугольника
+        /// </summary>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
         public Rectangle(double side1, double side2)
         {
-            this.side1 = side1;
-            this.side2 = side2;
+            this._side1 = side1;
+            this._side2 = side2;
         }
         
         /// <summary>
@@ -22,7 +29,7 @@ namespace ConsoleApp1
         /// <returns></returns>
         public double AreaCalculator()
         {
-           double result = side1 * side2;
+           double result = _side1 * _side2;
            return result;
         }
         
@@ -31,11 +38,14 @@ namespace ConsoleApp1
         /// </summary>
         /// <returns></returns>
        public double PerimetrCalculate()
-       {
-           double result = 2*(side1 + side2);
-           return result;
-       }
+        {
+            double result = 2 * (_side1 + _side2);
+            return result;
+        }
 
+        /// <summary>
+        /// Свойство площади
+        /// </summary>
         public double Area
         {
             get
@@ -44,6 +54,9 @@ namespace ConsoleApp1
             }
         }
 
+        /// <summary>
+        /// Свойство периметр
+        /// </summary>
         public double Perimetr
         {
             get
