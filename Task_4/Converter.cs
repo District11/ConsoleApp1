3625dd573bbb45ab9910ace5fb8bdbff28f567ff
District_11 <baira.98@mail.ru>
2021-09-29 15:15:17 +0400

    2 Задание 2 листа

---
+++ Task_4/Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_4
+{
+    public class Converter
+    {
+        public double _usd { get; set; }
+        public double _eur { get; set; }
+        public double _rub { get; set; }
+        public double _uah { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="usd"></param>
+        /// <param name="eur"></param>
+        /// <param name="rub"></param>
+        public ConverterTo(double usd, double eur, double rub)
+        {
+            this._usd = 26.54 ;
+            this._eur = 31.04;
+            this._rub = 0.37;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Ваши гривны: {_uah} = EUR:{_eur}, USD:{_usd}, RUB:{_rub}");
+        }
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Выберите действие: конвертация гривны в:" +
+                              "\n1. Конвертация гривны в Евро " +
+                              "\n2. Конвертация гривны в Доллары " +
+                              "\n3. Конвертация гривны в Рубли" +
+                              "\n4. Конвертация Евро из гривны " +
+                              "\n5. Конвертация Доллары из гривны" +
+                              "\n6. Конвертация Рубля из гривны ");
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество гривен");
+            Converter converter = new Converter(0.038, 0.032, 2.73);
+            Converter converter1 = new Converter(Convert.ToDouble(Console.ReadLine()));
+            switch (number)
+            {
+                case 1:
+                    converter1._eur = converter._eur * converter1._uah;
+                    converter1.Info();
+                    break;
+                case 2:
+                    converter1._usd = converter._usd * converter1._uah;
+                    converter1.Info();
+                    break;
+                case 3:
+                    converter1._rub = converter._rub * converter1._uah;
+                    converter1.Info();
+                    break;
+                case 4:
+                    converter._eur = converter1._uah / converter._eur;
+                    converter1.Info();
+                    break;
+                case 5:
+                    converter._usd = converter1._uah / converter._usd;
+                    converter1.Info();
+                    break;
+                case 6:
+                    converter._rub = converter1._uah / converter._rub;
+                    converter1.Info();
+                    break;
+                default:
+                    Console.WriteLine("Повторите ввод");
+                    break;
+            }
+        }
+    }
+}
