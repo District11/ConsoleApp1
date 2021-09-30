04f2e2e760db117665c6cea49d708330609ec691
District_11 <baira.98@mail.ru>
2021-10-01 03:35:43 +0400

    Выполненое задание 3 с листа 2

---
+++ Task_3_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_3_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Создание экземпляра
+            Employee employee = new Employee("Борис", "Бритва");
+            employee.Position = "manager";
+            employee.Expirience = 1; 
+
+            //Вывод результата
+            Console.WriteLine(employee.Surname + " " + employee.Name + " " + employee.Position);
+            employee.ShowPay();
+
+            Console.ReadKey();
+        }
+    }
+}
