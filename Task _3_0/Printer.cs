f2f6786001d6a3c897ca2dcad3cfa8113e6ce6ca
District_11 <baira.98@mail.ru>
2021-10-01 16:46:23 +0400

    Выполненое 1 задание из 3 листа

---
+++ Task _3_0/Printer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task__3_0
+{
+    public class ConsolePrinter: Printer
+    {
+        public ConsolePrinter(ConsoleColor color) : base(color)
+        {
+            
+        }
+    }
+    
+    public class Printer
+    {
+        /// <summary>
+        /// Поле цвета
+        /// </summary>
+        public ConsoleColor color;
+
+        /// <summary>
+        /// Конструктор для цвета
+        /// </summary>
+        /// <param name="color">Цвет</param>
+        public Printer(ConsoleColor color)
+        {
+            this.color = color;
+        }
+        
+        
+        public void Print(string value)
+        {
+            Console.ForegroundColor = color; //Задает цвет текста консоли.
+            Console.WriteLine(value);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
