59017ba8fcc9fdf75582ef8c42c6cf6368ec9318
District_11 <baira.98@mail.ru>
2021-10-01 10:25:15 +0400

    Исправление маленьких недочётов и добавление комментариев

--- Task_4/Converter.cs
+++ Task_4/Converter.cs
@@ -8,16 +8,16 @@ namespace Task_4
     public class Converter
     {
         /// <summary>
-        /// 
+        /// Поле валют
         /// </summary>
-        public double _usd, _eur, _rub, sum;
+        private double _usd, _eur, _rub, sum;
 
         /// <summary>
-        /// Метод для конвертации валюты
+        /// Конструктор для валюты валюты
         /// </summary>
-        /// <param name="usd"></param>
-        /// <param name="eur"></param>
-        /// <param name="rub"></param>
+        /// <param name="usd">Доллар</param>
+        /// <param name="eur">Евро</param>
+        /// <param name="rub">Рубли</param>
         public Converter(double usd, double eur, double rub)
         {
             //Инциализация валюты
