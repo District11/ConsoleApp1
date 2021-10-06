69745b78834d413c3eb0aec77b3afd9806b6c1f6
District_11 <baira.98@mail.ru>
2021-10-06 13:13:18 +0400

    Выполненное 4 задание из 3 листа

---
+++ Task_4_3/ProDocumentWorker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_4_3
+{
+    /// <summary>
+    /// Класс ProDocumenWorker
+    /// </summary>
+    public class ProDocumentWorker:DocumentWorker
+    {
+        /// <summary>
+        /// Метод редактирования в версии Про
+        /// </summary>
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+
+        /// <summary>
+        /// Метод сохранения в версии Про
+        /// </summary>
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+}
