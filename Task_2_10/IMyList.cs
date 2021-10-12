d0c894c7201b159a2526c459b3f6a3e12e112335
District11 <baira.98@mail.ru>
2021-10-13 00:02:23 +0400

    Выполненое задание 2_10

---
+++ Task_2_10/IMyList.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_2_10
+{
+    /// <summary>
+    /// Интерфейс MyList
+    /// </summary>
+    interface IMyList<T>
+    {
+        /// <summary>
+        /// Добавление элемента в массив
+        /// </summary>
+        /// <param name="item">Новый элемент массива</param>
+        void Add(T item);
+
+        /// <summary>
+        /// Индексатор массива
+        /// </summary>
+        /// <param name="index">Индекс</param>
+        /// <returns></returns>
+        T this[int index] { get; }
+        
+        /// <summary>
+        /// Метод для поулчения длинны массива
+        /// </summary>
+        int Counter { get; }
+
+        /// <summary>
+        /// Метод удаления массива
+        /// </summary>
+        void Remove();
+
+        /// <summary>
+        /// Метод для поиска элемента в массиве
+        /// </summary>
+        /// <param name="item">Элемент поиска</param>
+        /// <returns></returns>
+        bool Contains(T item);
+    }
+}
